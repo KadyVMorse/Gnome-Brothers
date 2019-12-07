4f096e98c626e8178a9664525dfd0d7d6ce05df0
KadyVMorse <kadmorse@uat.edu>
2019-12-07 01:31:45 -0700

    Added moe

---
+++ Orginal-master/UAT Brothers/Assets/Scrpts/EnemyDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    //finds the player 
+    private Player player;
+
+    void Start()
+    {
+        //finds the player tag 
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // On triger with the spikes the player will take 3 damage and move 
+        if (col.CompareTag("Player"))
+        {
+            player.Damage(3);
+
+            StartCoroutine(player.Knockback(0.02f, 350, player.transform.position));
+        }
+    }
+}
---
+++ Orginal-master/UAT Brothers/Assets/Scrpts/SnakeAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeAI : MonoBehaviour
+{
+    //Integers
+    public int curhealth;
+    public int maxhealth;
+
+    //Floats
+    public float distance;
+    public float wakeRange;
+
+    //Booleans
+    public bool awake;
+
+    //refernces
+    public Animator anim;
+    public Transform target;
+    public Transform attackPointLeft;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+     void Start()
+    {
+        curhealth = maxhealth;
+    }
+
+    void Update()
+    {
+        anim.SetBool("Awake", awake);
+
+        RangeCheck();
+    }
+
+    void RangeCheck()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+
+        if(distance < wakeRange)
+        {
+            awake = true;
+        }
+        if(distance > wakeRange)
+        {
+            awake = false;
+        }
+    }
+}
