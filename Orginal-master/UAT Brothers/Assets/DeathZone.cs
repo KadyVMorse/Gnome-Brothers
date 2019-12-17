fe23fc4b7aa517cddc4fd849f2b2f164491bc73a
KadyVMorse <kadmorse@uat.edu>
2019-12-16 21:33:08 -0700

    Added DeathZone

---
+++ Orginal-master/UAT Brothers/Assets/DeathZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        //Reload to the begging of the Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+  
+}
